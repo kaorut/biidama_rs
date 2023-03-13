f5f879a851b467cfd6d1c1778a3534c07b224a43
kaoru <kaorut@mc.pikara.ne.jp>
2023-03-13 13:39:51 +0900

    Change event to update

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,18 @@ use nannou::prelude::*;
 struct Model {}
 
 fn main() {
-    nannou::app(model).event(event).simple_window(view).run();
+    nannou::app(model).update(update).simple_window(view).run();
 }
 
 fn model(_app: &App) -> Model {
     return Model {};
 }
 
-fn event(_app: &App, _model: &mut Model, _event: Event) {}
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
 fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(WHITE);
+
     draw.to_frame(app, &frame).unwrap();
 }
