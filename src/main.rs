272cc3798036d5cd4c0fd3f6c1b79e4135b37157
kaoru <kaorut@mc.pikara.ne.jp>
2023-03-18 07:54:52 +0900

    Add balls

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn model(_app: &App) -> Model {
     biidamas.push(Biidama {
         position: Vec2::new(0.0, 0.0),
         radius: 50.0,
-        velocity: Vec2::new(2.0, 1.0),
+        velocity: Vec2::new(10.0, 5.0),
         color: Rgba::new(1.0, 0.0, 0.0, 1.0),
     });
     biidamas.push(Biidama {
@@ -35,6 +35,18 @@ fn model(_app: &App) -> Model {
         radius: 30.0,
         velocity: Vec2::new(20.0, -5.0),
         color: Rgba::new(0.0, 0.0, 1.0, 1.0),
+    });
+    biidamas.push(Biidama {
+        position: Vec2::new(30.0, 200.0),
+        radius: 40.0,
+        velocity: Vec2::new(-20.0, 10.0),
+        color: Rgba::new(0.0, 0.5, 0.0, 1.0),
+    });
+    biidamas.push(Biidama {
+        position: Vec2::new(40.0, 50.0),
+        radius: 20.0,
+        velocity: Vec2::new(2.0, -3.0),
+        color: Rgba::new(1.0, 0.0, 1.0, 1.0),
     });
     return Model {
         biidamas: biidamas,
