10e2e5194fd211491d5b1af0b5dde1e4fe9e9c1c
kaoru <kaorut@mc.pikara.ne.jp>
2023-03-18 07:51:58 +0900

    Implement collision

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn model(_app: &App) -> Model {
         color: Rgba::new(1.0, 0.0, 0.0, 1.0),
     });
     biidamas.push(Biidama {
-        position: Vec2::new(20.0, 50.0),
+        position: Vec2::new(120.0, 150.0),
         radius: 30.0,
         velocity: Vec2::new(20.0, -5.0),
         color: Rgba::new(0.0, 0.0, 1.0, 1.0),
@@ -42,10 +42,10 @@ fn model(_app: &App) -> Model {
     };
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {
+fn update(_app: &App, model: &mut Model, _update: Update) {
     let frame_rect = _app.window_rect();
     let velocity_unit = _update.since_last.as_secs_f32() * 100.0;
-    for b in &mut _model.biidamas {
+    for b in &mut model.biidamas {
         (b.position.x, b.velocity.x) = reflect_on_wall(
             b.position.x,
             b.radius,
@@ -63,17 +63,25 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {
             frame_rect.top(),
         );
     }
-    for i in 0.._model.biidamas.len() - 1 {
-        for j in i + 1.._model.biidamas.len() {
-            let b1 = &_model.biidamas[i];
-            let b2 = &_model.biidamas[j];
-            if collided(b1, b2) {
-                println!("collided: {} - {}", i, j);
+    for i in 0..model.biidamas.len() - 1 {
+        for j in i + 1..model.biidamas.len() {
+            let v1: Vec2;
+            let v2: Vec2;
+            {
+                let b1 = &model.biidamas[i];
+                let b2 = &model.biidamas[j];
+                if !collided(b1, b2) {
+                    continue;
+                }
+                v1 = collide(b1, b2, 1.0);
+                v2 = collide(b2, b1, 1.0);
             }
+            model.biidamas[i].velocity = v1;
+            model.biidamas[j].velocity = v2;
         }
     }
 
-    _model.fps = (1.0 / _update.since_last.as_secs_f32()) as usize;
+    model.fps = (1.0 / _update.since_last.as_secs_f32()) as usize;
 }
 
 fn reflect_on_wall(
@@ -109,12 +117,28 @@ fn collided(biidama1: &Biidama, biidama2: &Biidama) -> bool {
     return squared_distance < squared_sum_of_radiuses;
 }
 
-fn view(app: &App, _model: &Model, frame: Frame) {
+fn collide(biidama1: &Biidama, biidama2: &Biidama, k: f32) -> Vec2 {
+    let m1 = biidama1._mass();
+    let m2 = biidama2._mass();
+    let v1 = biidama1.velocity;
+    let v2 = biidama2.velocity;
+
+    return Vec2::new(
+        collide_impl(m1, m2, v1.x, v2.x, k),
+        collide_impl(m1, m2, v1.y, v2.y, k),
+    );
+}
+
+fn collide_impl(m1: f32, m2: f32, v1: f32, v2: f32, k: f32) -> f32 {
+    return ((m1 - k * m2) * v1 + (k + 1.0) * m2 * v2) / (m1 + m2);
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     let window_rect = app.window_rect();
     draw.background().color(WHITE);
 
-    for b in &_model.biidamas {
+    for b in &model.biidamas {
         draw.ellipse()
             .x_y(b.position.x, b.position.y)
             .w_h(b.radius * 2.0, b.radius * 2.0)
@@ -122,7 +146,7 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     }
 
     let text_area = Rect::from_w_h(window_rect.w(), 24.0).top_left_of(window_rect.pad(0.0));
-    draw.text(std::format!("FPS: {}", _model.fps).as_str())
+    draw.text(std::format!("FPS: {}", model.fps).as_str())
         .color(BLACK)
         .left_justify()
         .xy(text_area.xy())
