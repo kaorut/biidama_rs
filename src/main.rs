cba4d4bb5aed3a10aa9d3d3c377fe90445305b8f
kaoru <kaorut@mc.pikara.ne.jp>
2023-03-18 10:26:11 +0900

    Change the velocities

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ struct Biidama {
 
 impl Biidama {
     fn _mass(&self) -> f32 {
-        return self.radius;
+        return (self.radius * self.radius).sqrt();
     }
 }
 
@@ -27,25 +27,25 @@ fn model(_app: &App) -> Model {
     biidamas.push(Biidama {
         position: Vec2::new(0.0, 0.0),
         radius: 50.0,
-        velocity: Vec2::new(10.0, 5.0),
+        velocity: Vec2::new(5.0, 2.0),
         color: Rgba::new(1.0, 0.0, 0.0, 1.0),
     });
     biidamas.push(Biidama {
         position: Vec2::new(120.0, 150.0),
         radius: 30.0,
-        velocity: Vec2::new(20.0, -5.0),
+        velocity: Vec2::new(10.0, -2.0),
         color: Rgba::new(0.0, 0.0, 1.0, 1.0),
     });
     biidamas.push(Biidama {
         position: Vec2::new(30.0, 200.0),
         radius: 40.0,
-        velocity: Vec2::new(-20.0, 10.0),
+        velocity: Vec2::new(-10.0, 5.0),
         color: Rgba::new(0.0, 0.5, 0.0, 1.0),
     });
     biidamas.push(Biidama {
         position: Vec2::new(40.0, 50.0),
         radius: 20.0,
-        velocity: Vec2::new(2.0, -3.0),
+        velocity: Vec2::new(1.0, -1.0),
         color: Rgba::new(1.0, 0.0, 1.0, 1.0),
     });
     return Model {
